f0f3dad65a6f990c6b7c01002cea48231dd6c895
Grace Amondi <miswa.grace@gmail.com>
2026-03-04 18:43:10 +0300

    ghcr implementation

--- mapviewer/docker-entrypoint.sh
+++ mapviewer/docker-entrypoint.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
+
+# Generate runtime .env file
+cat <<EOF > /app/.env
+ANALYTICS_PROPERTY_ID=${ANALYTICS_PROPERTY_ID}
+BITLY_TOKEN=${BITLY_TOKEN}
+GOOGLE_CUSTOM_SEARCH_CX=${GOOGLE_CUSTOM_SEARCH_CX}
+GOOGLE_SEARCH_API_KEY=${GOOGLE_SEARCH_API_KEY}
+BASE_PATH=${BASE_PATH}
+ASSET_PREFIX=${ASSET_PREFIX}
+CMS_API=${CMS_API}
+EOF
 
 # copy .next files to enable connecting mounted volumes to static
 mkdir -p /app/nginx/.next
 cp -r /app/.next/. /app/nginx/.next/
 
-yarn start
-
-exec "$@"
+# Start Next.js
+exec yarn start
